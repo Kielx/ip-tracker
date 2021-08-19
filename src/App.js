5f39a82a2a5ee8a422618da1072d5aed8d2c0520
Kielx <kiel66@gmail.com>
2021-08-19 21:07:20 +0200

    Add basic data fetching logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [geoIP, setGeoIP] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    sessionStorage.getItem("geoIP") &&
+      setGeoIP(JSON.parse(sessionStorage.getItem("geoIP")));
+  }, []);
+
+  useEffect(() => {
+    sessionStorage.setItem("geoIP", JSON.stringify(geoIP));
+  }, [geoIP]);
+
+  async function getGeoIP(link) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_GEO_API_KEY}`
+      );
+      let data = await response.json();
+      setLoading(false);
+      console.log(data);
+      return setGeoIP(data);
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  }
+
   return (
-    <div className="bg-blue-400 h-screen grid">
-      <div className="w-3/4 my-auto ml-20">
-        <h1 className="text-5xl font-bold mb-10 text-white">
+    <div className="bg-blue-500 h-screen grid">
+      <div className="w-[50%] my-auto ml-20">
+        <h1 className="text-[5rem] font-bold mb-10 text-white">
           JIT mode is cool
         </h1>
         <p className="text-white">
@@ -11,6 +41,9 @@ function App() {
           at officiis tempore, magnam non debitis fugit unde alias id quidem
           necessitatibus.
         </p>
+        <button onClick={getGeoIP} className="bg-red-600 w-[100px] h-[50px]">
+          Get IP
+        </button>
       </div>
     </div>
   );
