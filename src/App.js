79311d4f87ebec9065bb935820db7e93d8d5f050
Kielx <kiel66@gmail.com>
2021-08-20 11:21:22 +0200

    Set up map to display local IP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Map from "./components/map";
 
 function App() {
   const [localIP, setLocalIP] = useState("");
@@ -11,7 +12,6 @@ function App() {
       try {
         const response = await fetch("https://jsonip.com/");
         let data = await response.json();
-        console.log(data);
         setLocalIP(data.ip);
         return;
       } catch (e) {
@@ -22,6 +22,24 @@ function App() {
     getLocalIP();
   }, []);
 
+  async function getGeoIP(IP) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_GEO_API_KEY
+          ? `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_GEO_API_KEY}&ipAddress=${IP}`
+          : `/.netlify/functions/getGeoIP?ip=${IP}`
+      );
+      let data = await response.json();
+      setLoading(false);
+      return setGeoIP(data);
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
+      return;
+    }
+  }
+
   useEffect(() => {
     localStorage.getItem("geoIP") &&
       setGeoIP(JSON.parse(localStorage.getItem("geoIP")));
@@ -30,25 +48,6 @@ function App() {
   useEffect(() => {
     localStorage.setItem("geoIP", JSON.stringify(geoIP));
   }, [geoIP]);
-
-  async function getGeoIP() {
-    setLoading(true);
-    try {
-      const response = await fetch(
-        process.env.REACT_APP_GEO_API_KEY
-          ? `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_GEO_API_KEY}`
-          : `/.netlify/functions/getGeoIP?ip=${localIP}`
-      );
-      let data = await response.json();
-      setLoading(false);
-      console.log(data);
-      return setGeoIP(data);
-    } catch (e) {
-      setLoading(false);
-      console.log(e);
-      return;
-    }
-  }
 
   return (
     <div className="bg-blue-500 h-screen grid">
@@ -66,10 +65,16 @@ function App() {
         <p className="text-white">{geoIP?.location?.country}</p>
         <p className="text-white">{geoIP?.location?.region}</p>
         <p className="text-white">{geoIP?.location?.city}</p>
-        <button onClick={getGeoIP} className="bg-red-600 w-[100px] h-[50px]">
+        <button
+          onClick={() => getGeoIP(localIP)}
+          className="bg-red-600 w-[100px] h-[50px]"
+        >
           Get IP
         </button>
       </div>
+      {geoIP ? (
+        <Map lat={geoIP?.location?.lat} lng={geoIP?.location?.lng}></Map>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/map.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const Map = ({ lat, lng }) => {
+  console.log(lat, lng);
+  const position = [lat, lng];
+
+  return (
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
